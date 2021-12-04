4d264cad034b795f1e8f598fceebb4560b94569f
den10b <den10b@yandex.ru>
2021-12-04 17:57:25 +0300

    Финалочка

--- Animal Detection/Form1.cs
+++ Animal Detection/Form1.cs
@@ -16,9 +16,10 @@ using Microsoft.Scripting.Hosting;
 
 namespace Animal_Detection
 {
+   
     public partial class Form1 : Form
     {
-
+        public string ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\wilDBase.mdf;Integrated Security=True";
 
 
         public Form1()
@@ -26,6 +27,109 @@ namespace Animal_Detection
             InitializeComponent();
             PopulateTreeView();
 // PopulateTreeView1();
+        }
+        public void PyLaunch()
+        {
+            ScriptEngine engine = Python.CreateEngine();
+            string filename = @"...\main.py";  //ФАЙЛ В ПАПКУ БИН
+            engine.ExecuteFile(filename);
+        }
+        public void InsertFiles(string path)         //Инсертит в БД
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            List<string> files = new List<string>();
+            foreach (FileInfo file in dir.GetFiles())                               //Перебираем файлы и запихиваем Адрес в Лист
+            {
+                files.Add(file.FullName);
+            }
+
+
+            SqlConnection Conn = new SqlConnection(ConStr);
+            Conn.Open();
+            foreach (string filename in files)
+            {
+                using (var cmd = Conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT  vid (vid_addr) Values (@filename)";
+                    cmd.Parameters.AddWithValue("@filename", filename);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            Conn.Close();
+        }
+
+
+
+
+        public void SortFiles(string path)
+        {
+            SqlConnection Conn = new SqlConnection(ConStr);
+            Conn.Open();
+            int count;
+            using (var cmd = Conn.CreateCommand())
+            {
+                cmd.CommandText = "SELECT  COUNT( * ) FROM  vid";
+                count = Int32.Parse(cmd.ExecuteScalar().ToString());
+            }
+
+            string goodPath = path + @"\Good";
+            string badPath = path + @"\Bad";
+            string soPath = path + @"\So-so";
+            if (!Directory.Exists(goodPath)) Directory.CreateDirectory(goodPath);
+            if (!Directory.Exists(badPath)) Directory.CreateDirectory(badPath);
+            if (!Directory.Exists(soPath)) Directory.CreateDirectory(soPath);
+            for (int i = 1; i <= count; i++)
+            {
+                using (var cmd = Conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT  vid_animal FROM  vid WHERE vid_id=@id";
+                    cmd.Parameters.AddWithValue("@id", i);
+                    char p = cmd.ExecuteScalar().ToString().First();
+
+                    cmd.CommandText = "SELECT  vid_addr FROM  vid WHERE vid_id=@id";
+                    cmd.Parameters.AddWithValue("@id", i);
+                    string addr = cmd.ExecuteScalar().ToString();
+                    FileInfo fileInf = new FileInfo(path);
+
+                    switch (p)
+                    {
+                        case 'T':
+                            if (fileInf.Exists) fileInf.CopyTo(goodPath);
+                            break;
+                        case 'F':
+                            if (fileInf.Exists) fileInf.CopyTo(badPath);
+                            break;
+                        case 'S':
+                            if (fileInf.Exists) fileInf.CopyTo(soPath);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            using (var cmd = Conn.CreateCommand())
+            {
+                cmd.CommandText = "TRUNCATE TABLE vid";
+                cmd.ExecuteNonQuery();
+            }
+
+            Conn.Close();
+        }
+
+
+        public int WaitNoNull()
+        {
+            int count2;
+            SqlConnection Conn = new SqlConnection(ConStr);
+            Conn.Open();
+            using (var cmd = Conn.CreateCommand())
+            {
+                cmd.CommandText = "SELECT  COUNT( * ) FROM  vid WHERE vid_animal IS NULL";
+                count2 = Int32.Parse(cmd.ExecuteScalar().ToString());
+            }
+            Conn.Close();
+            return count2;
         }
 
         private void PopulateTreeView()
@@ -106,6 +210,28 @@ namespace Animal_Detection
             }
 
             listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = null;
+            using (var dialog = new FolderBrowserDialog())
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)        //Диалог выбора каталога
+                    path = dialog.SelectedPath;
+
+            InsertFiles(path); // Инсертит в БД
+
+            PyLaunch();        // Запускет питоновский файл из Bin
+
+            int count;
+            do
+            {
+                count = WaitNoNull();
+            } while (count != 0);
+
+
+            SortFiles(path);   // Запускает пост-обработочную сортировку
+
         }
     }
 }
